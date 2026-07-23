fba4b20db546760087e065110813fb4d7e4b8c76
Ansar <ansarmaksutov10@gmail.com>
2026-07-23 21:21:40 +0500

    по фиксил forgotPassword кнопку восстановить

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -124,6 +124,14 @@ const App = () => {
               </Modal>
             }
           />
+          <Route
+            path='/profile/orders/:id'
+            element={
+              <Modal title='Детали заказа' onClose={handleCloseModal}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
         </Routes>
       )}
     </div>
--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -2,15 +2,28 @@ import { FC, useState, SyntheticEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { ForgotPasswordUI } from '@ui-pages';
+import { forgotPasswordThunk } from '../../slices/resetPassword';
+import { AppDispatch } from '../../services/store';
+import { useDispatch } from 'react-redux';
 
 export const ForgotPassword: FC = () => {
   const [email, setEmail] = useState('');
   const [error, setError] = useState<Error | null>(null);
 
+  const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(forgotPasswordThunk({ email }))
+      .unwrap()
+      .then(() => {
+        localStorage.setItem('resetPassword', 'true');
+        navigate('/reset-password', { replace: true });
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      });
   };
 
   return (
